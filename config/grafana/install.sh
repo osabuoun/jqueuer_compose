0f23c18a323bf85362efd0e80fedb378aae5b959
Osama <oabuoun1>
2018-05-24 13:47:50 +0100

    first

---
+++ config/grafana/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+result="0"
+while [ $result -eq "0" ]; do
+	(echo > /dev/tcp/localhost/3000) >/dev/null 2>&1  && result=1 || result=0
+	echo "Checking Grafana port"
+  	sleep 1
+done
+
+echo "Grafana is up - executing command"
+curl --header "Content-Type: application/json" --data @./config/grafana/datasource.json http://admin:jqueuer@127.0.0.1:3000/api/datasources/
+curl --header "Content-Type: application/json" --data @./config/grafana/dashboard.json http://admin:jqueuer@127.0.0.1:3000/api/dashboards/db
+curl --header "Content-Type: application/json" --data @./config/grafana/job_dashboard.json http://admin:jqueuer@127.0.0.1:3000/api/dashboards/db
